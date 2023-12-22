2274afb0f57fcaa66137ef9509e85d10dfd285f9
Lidanny Gonzalez <gonzalezlidanny@gmail.com>
2023-12-21 23:26:50 -0300

    2da Pre entrga

--- trivia.js
+++ trivia.js
@@ -1,4 +1,5 @@
-const preguntas = [
+const categorias = {
+  marvel: [
     {
       pregunta: "¿Cuál es el nombre real de Iron Man?",
       opciones: ["Tony Stark", "Bruce Banner", "Steve Rogers", "Peter Parker"],
@@ -24,54 +25,96 @@ const preguntas = [
       opciones: ["Bruce Banner", "Tony Stark", "Peter Parker", "Scott Lang"],
       respuestaCorrecta: "Bruce Banner"
     }
-  ];
-  
-  function jugarTrivia() {
-    let puntaje = 0;
-  
-    for (let i = 0; i < preguntas.length; i++) {
-      const preguntaActual = preguntas[i];
-  
-      let respuestaUsuario;
-      do {
-        respuestaUsuario = prompt(
-          `${preguntaActual.pregunta}\nOpciones:\n${preguntaActual.opciones.map((opcion, index) => `${index + 1}. ${opcion}`).join('\n')}\nIngresa el número de tu respuesta:`
-        );
-      
-        if (!/^[1-4]$/.test(respuestaUsuario)) {
-          alert("Por favor, ingresa un número válido (1-4).");
-        }
-      } while (!/^[1-4]$/.test(respuestaUsuario));
-  
-      if (verificarRespuesta(respuestaUsuario, preguntaActual)) {
-        puntaje++;
-        console.log("¡Correcto!\n");
-      } else {
-        console.log(`Incorrecto. La respuesta correcta es: ${preguntaActual.respuestaCorrecta}\n`);
+  ],
+  otraCategoria: [
+    {
+      pregunta: "¿En qué año se lanzó el primer cómic de Spider-Man?",
+      opciones: ["1962", "1975", "1989", "2001"],
+      respuestaCorrecta: "1962"
+    },
+    {
+      pregunta: "¿Cuál es el planeta natal de Gamora en Guardianes de la Galaxia?",
+      opciones: ["Tatooine", "Mars", "Zen-Whoberi", "Alderaan"],
+      respuestaCorrecta: "Zen-Whoberi"
+    },
+    {
+      pregunta: "¿Cuál es el nombre real de Black Widow?",
+      opciones: ["Natasha Romanoff", "Wanda Maximoff", "Maria Hill", "Carol Danvers"],
+      respuestaCorrecta: "Natasha Romanoff"
+    },
+    {
+      pregunta: "¿Qué actor interpreta a Deadpool en las películas?",
+      opciones: ["Chris Hemsworth", "Ryan Reynolds", "Robert Downey Jr.", "Chris Evans"],
+      respuestaCorrecta: "Ryan Reynolds"
+    },
+    {
+      pregunta: "¿Cuál es el villano principal en la película 'Avengers: Infinity War'?",
+      opciones: ["Loki", "Thanos", "Ultron", "Red Skull"],
+      respuestaCorrecta: "Thanos"
+    }
+  ]
+};
+
+function jugarTrivia() {
+  let puntaje = 0;
+  const inicioTiempo = new Date();
+
+  const preguntasMarvelAleatorias = obtenerPreguntasAleatorias(categorias.marvel);
+  const preguntasOtraCategoriaAleatorias = obtenerPreguntasAleatorias(categorias.otraCategoria);
+
+  const preguntasAleatorias = preguntasMarvelAleatorias.concat(preguntasOtraCategoriaAleatorias);
+
+  for (let i = 0; i < preguntasAleatorias.length; i++) {
+    const preguntaActual = preguntasAleatorias[i];
+
+    let respuestaUsuario;
+    do {
+      respuestaUsuario = prompt(
+        `${preguntaActual.pregunta}\nOpciones:\n${preguntaActual.opciones.map((opcion, index) => `${index + 1}. ${opcion}`).join('\n')}\nIngresa el número de tu respuesta:`
+      );
+
+      if (!/^[1-4]$/.test(respuestaUsuario)) {
+        alert("Por favor, ingresa un número válido (1-4).");
       }
-    }
-  
-    console.log(`Tu puntaje final es: ${puntaje} de ${preguntas.length}`);
-  
-    if (puntaje < 2) {
-      console.log("Parte del viaje es el final. (Tony Stark) No sabes mucho de Marvel. ¡Sigue intentándolo!");
-    } else if (puntaje <= 4) {
-      console.log("¡Buen intento! Conoces algo sobre Marvel ¡Wakanda Forever!");
+    } while (!/^[1-4]$/.test(respuestaUsuario));
+
+    if (verificarRespuesta(respuestaUsuario, preguntaActual)) {
+      puntaje++;
+      alert("¡Correcto!");
     } else {
-      console.log("¡Felicidades! Podrías hacer esto, todo el día. Eres un experto en Marvel.");
+      alert(`Incorrecto. La respuesta correcta es: ${preguntaActual.respuestaCorrecta}`);
     }
   }
-  
-  function verificarRespuesta(respuestaUsuario, pregunta) {
-    const respuestaCorrecta = pregunta.respuestaCorrecta;
-    const indiceRespuestaUsuario = parseInt(respuestaUsuario) - 1;
-  
-    return (
-      indiceRespuestaUsuario >= 0 &&
-      indiceRespuestaUsuario < pregunta.opciones.length &&
-      pregunta.opciones[indiceRespuestaUsuario] === respuestaCorrecta
-    );
+
+  const finTiempo = new Date();
+  const tiempoTranscurrido = (finTiempo - inicioTiempo) / 1000;
+
+  alert(`Tu puntaje final es: ${puntaje} de ${preguntasAleatorias.length}.`);
+  alert(`Tiempo transcurrido: ${tiempoTranscurrido.toFixed(2)} segundos.`);
+
+  if (puntaje < 2) {
+    alert("Parte del viaje es el final. (Tony Stark) No sabes mucho de Marvel. ¡Sigue intentándolo!");
+  } else if (puntaje <= 4) {
+    alert("¡Buen intento! Conoces algo sobre Marvel ¡Wakanda Forever!");
+  } else {
+    alert("¡Felicidades! Podrías hacer esto, todo el día. Eres un experto en Marvel.");
   }
-  
-  jugarTrivia();
- 
+}
+
+function verificarRespuesta(respuestaUsuario, pregunta) {
+  const respuestaCorrecta = pregunta.respuestaCorrecta;
+  const indiceRespuestaUsuario = parseInt(respuestaUsuario) - 1;
+
+  return (
+    indiceRespuestaUsuario >= 0 &&
+    indiceRespuestaUsuario < pregunta.opciones.length &&
+    pregunta.opciones[indiceRespuestaUsuario] === respuestaCorrecta
+  );
+}
+
+function obtenerPreguntasAleatorias(categoria) {
+  const preguntasAleatorias = categoria.sort(() => Math.random() - 0.5);
+  return preguntasAleatorias.slice(0, 5); 
+}
+
+jugarTrivia();
